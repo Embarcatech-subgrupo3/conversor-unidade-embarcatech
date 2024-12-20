ae2b1efa64bc62628d47ea49da1222c5303fb3c0
Luiz Gustavo <luizgvv@gmail.com>
2024-12-20 09:54:07 -0300

    primeiro commit

---
+++ conversor_armazenamento.c
@@ -0,0 +1,12 @@
+// conversor_armazenamento.c
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    printf("\nPROGRAMA PARA CONVERTER DADOS DIGITAIS\n");
+    sleep(1);
+    printf("Unidades suportadas: bits (b), bytes (B), KB, MB, GB, TB\n");
+
+    return 0;
+}
