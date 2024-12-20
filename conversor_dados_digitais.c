ab6cd7697300823fe22e2872ad44b2e194fa1dac
Luiz Gustavo do Valle <luizgvv@gmail.com>
2024-12-20 13:43:38 -0300

    Função de Conversão

--- conversor_dados_digitais.c
+++ conversor_dados_digitais.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+double converterDados(double valor, char unidade_origem[3], char unidade_destino[3]);
 
 int main(void)
 {
+char continuar;
+    do {
     printf("\nPROGRAMA PARA CONVERTER DADOS DIGITAIS\n");
-    sleep(1);
     printf("Unidades suportadas: bits (b), bytes (B), KB, MB, GB, TB\n");
 
     double valor;
@@ -19,5 +24,29 @@ int main(void)
     printf("Para qual unidade deseja converter? (b, B, KB, MB, GB, TB): ");
     scanf("%2s", unidade_destino);
 
+    double resultado = converterDados(valor, unidade_origem, unidade_destino);
+
+        if (resultado < 0)
+        {
+            printf("Unidade inválida. Encerrando...\n");
+            exit(EXIT_FAILURE);
+        }
+    printf("\n%.2f %s equivale a %.3f %s\n", valor, unidade_origem, resultado, unidade_destino);
+
+        // Pergunta ao usuário se deseja fazer outra conversão
+        printf("\nDeseja realizar outra conversão? (S para Sim / N para Não): ");
+        scanf(" %c", &continuar);
+        continuar = toupper(continuar);
+
+    } while (continuar == 'S');
+
+    printf("\nObrigado por usar o programa! Encerrando...\n");
+
+
     return 0;
+}
+
+double converterDados(double valor, char unidade_origem[3], char unidade_destino[3])
+{
+    return valor; // Função de conversão será implementada na próxima etapa
 }
