08754536fd7bd6c264eca52e0766638ecf9ee1aa
Luiz Gustavo <luizgvv@gmail.com>
2024-12-22 21:16:06 -0300

    adicionado o teste unitário junto com o código original

--- conversor_dados_digitais.c
+++ conversor_dados_digitais.c
@@ -1,3 +1,4 @@
+// conversor_dados_digitais.c
 #include <stdio.h>
 #include <unistd.h>
 #include <ctype.h>
@@ -8,39 +9,65 @@ double converterDados(double valor, char unidade_origem[3], char unidade_destino
 
 int main(void)
 {
+    char continuar;
+
     FILE *entrada = fopen("entradas.txt", "r");
     FILE *saida = fopen("saidas.txt", "w");
 
     if (entrada == NULL || saida == NULL)
     {
-        printf("Erro ao abrir arquivo.\n");
+        printf("Erro ao abrir arquivo.");
         return 1;
     }
 
-    fprintf(saida, "Resultados das conversões de dados digitais:\n");
+    fprintf(saida, "Resultados da conversão:\n");
 
     double valor;
     char unidade_origem[3], unidade_destino[3];
 
-    while (fscanf(entrada, "%lf %2s %2s\n", &valor, unidade_origem, unidade_destino) == 3)
+    while (fscanf(entrada, "%lf %2s %2s", &valor, unidade_origem, unidade_destino) == 3)
     {
-        // Chama a função de conversão
         double resultado = converterDados(valor, unidade_origem, unidade_destino);
 
         if (resultado < 0)
         {
-            fprintf(saida, "Unidade inválida para %s para %s. Conversão não realizada.\n", unidade_origem, unidade_destino);
+            fprintf(saida, "Unidade inválida: %s ou %s\n", unidade_origem, unidade_destino);
+            continue;
         }
-        else
-        {
-            fprintf(saida, "%.2f %s equivale a %.3f %s\n", valor, unidade_origem, resultado, unidade_destino);
-        }
+
+        fprintf(saida, "%.2f %s equivale a %.3f %s\n", valor, unidade_origem, resultado, unidade_destino);
     }
 
     fclose(entrada);
     fclose(saida);
 
-    printf("Conversões completas. Resultados foram gravados em 'saidas.txt'.\n");
+    do {
+        printf("\nPROGRAMA PARA CONVERTER DADOS DIGITAIS\n");
+        printf("Unidades suportadas: bits (b), bytes (B), KB, MB, GB, TB\n");
+
+        printf("\nInsira o valor e a unidade de origem (ex: 1024 KB): ");
+        scanf("%lf %2s", &valor, unidade_origem);
+
+        printf("Para qual unidade deseja converter? (b, B, KB, MB, GB, TB): ");
+        scanf("%2s", unidade_destino);
+
+        double resultado = converterDados(valor, unidade_origem, unidade_destino);
+
+        if (resultado < 0)
+        {
+            printf("Unidade inválida. Encerrando...\n");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("\n%.2f %s equivale a %.3f %s\n", valor, unidade_origem, resultado, unidade_destino);
+
+        printf("\nDeseja realizar outra conversão? (S para Sim / N para Não): ");
+        scanf(" %c", &continuar);
+        continuar = toupper(continuar);
+
+    } while (continuar == 'S');
+
+    printf("\nObrigado por usar o programa! Encerrando...\n");
 
     return 0;
 }
